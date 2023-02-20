2eb0d27f2d47e6b7cb2636622df9f44987312d0c
Gaurav Kumar Thakur <gauravkrthakur07@gmail.com>
2023-02-20 19:35:59 +0530

    Added Insert function and printFunc to SLL(Linkedlist)

--- LinkedList/SLL.java
+++ LinkedList/SLL.java
@@ -1,11 +1,13 @@
 class SLL {
     Node head = null;
+    public int size;
     class Node{ 
         int data;
         Node next;
         public Node(int data){
             this.data = data;
             this.next = null;
+            size++;
         }
     }
 
@@ -45,8 +47,30 @@ class SLL {
             curr = curr.next;
         }
 
-        System.out.print("null");
+        System.out.print("null\n");
 
+    }
+
+    public void insert(int index,int data){
+
+        if(index>size){
+            System.out.println("Invalid");
+        }
+        Node newNode = new Node(data);
+
+        Node curr = head;
+        for(int i=0;i<index-1;i++){
+            curr = curr.next;
+        }
+
+        newNode.next = curr.next;
+        curr.next = newNode;
+
+
+    }
+
+    public int getSize(){
+        return size;
     }
 
 
@@ -58,6 +82,14 @@ class SLL {
         list.append(15);
         list.append(33);
         list.append(21);
+        list.printSLL();
+        System.out.println(list.getSize()); 
+        list.prepend(1);
+        list.printSLL();
+        System.out.println(list.getSize()); 
+        list.prepend(5);
+        list.printSLL();
+        list.insert(2, 11);
         list.printSLL();
     }
 }
